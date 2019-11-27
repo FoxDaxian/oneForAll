98fa8478cb9d2b6a0d467a4f32d72c47dbef0eed
FoxDaxian <945039036@qq.com>
2019-11-27 20:42:24 +0800

    fix: lerna增加demo

--- scripts/__test__/index.test.js
+++
@@ -1,5 +0,0 @@
-// const utils = require('../utils');
-
-// test('sum', () => {
-//     expect(utils.sum(1, 2)).toBe(3);
-// });
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,9 @@
 const glob = require('glob');
 const path = require('path');
-
-// function sum(a, b) {
-//     return a + b;
-// }
 function getPkg() {
     return glob.sync(path.join(process.cwd(), 'packages', '*'));
 }
 
 module.exports = {
-    // sum,
     getPkg
 };
